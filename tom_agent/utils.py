8969e66d1bc8e2b74b23df70fc85a9f3d1500fa2
lumen <lumen2004.scholar@gmail.com>
2024-12-13 09:56:15 +0800

    Chore: Add count_total_cards.

--- tom_agent/utils.py
+++ tom_agent/utils.py
@@ -1,1 +1,15 @@
 ID2COLOR = "RYGWB"
+
+
+def count_total_cards(num_colors: int, num_ranks: int):
+    if num_ranks == 1:  # +3
+        return 3 * num_colors
+    elif num_ranks == 2:  # +2
+        return 5 * num_colors
+    elif num_ranks == 3:  # +2
+        return 7 * num_colors
+    elif num_ranks == 4:  # +2
+        return 9 * num_colors
+    else:  # +1
+        return 10 * num_colors
+    
